3ada16def52f02fdda2baa6acde387d2ec377da5
billdred <billdred16@gmail.com>
2019-02-11 22:45:27 -0700

    First Push

---
+++ builder.js
@@ -0,0 +1,14 @@
+var length = o;
+length = Math.floor(Math.random() * 25) +12;
+var bFlat = [];
+var cNat = [];
+var fNat = [];
+var trombone = [];
+var count = 0;
+while (count < length){
+  bFlat.push("<img src='" +Math.floor(Math.random() * 9) + ".png' />");
+  cNat.push("<img src='" +Math.floor(Math.random() * 9) + ".png' />");
+  fNat.push("<img src='" +Math.floor(Math.random() * 9) + ".png' />");
+  trombone.push("<img src='" +Math.floor(Math.random() * 9) + ".png' />");
+  count ++}
+}
